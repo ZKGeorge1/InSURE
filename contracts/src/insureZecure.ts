f06b8348d40ce16f481d30a101ebcedbe40bd449
ZKGeorge <37027892+ZKGeorge1@users.noreply.github.com>
2023-10-21 18:33:30 +0100

    Update insureZecure.ts

--- contracts/src/insureZecure.ts
+++ contracts/src/insureZecure.ts
@@ -80,8 +80,8 @@ export class insureZecure extends SmartContract {
 
     const currentRequirementsHash = this.verifiedRequirementsHash.get()
 
-    // console.log('incoming requirements hash: ', requirementsHashToCheck)
-    // console.log('current requirements hash: ', currentRequirementsHash)
+    console.log('incoming requirements hash: ', requirementsHashToCheck)
+    console.log('current requirements hash: ', currentRequirementsHash)
 
     currentRequirementsHash.assertGreaterThan(Field(0));
     this.verifiedRequirementsHash.assertEquals(requirementsHashToCheck);
